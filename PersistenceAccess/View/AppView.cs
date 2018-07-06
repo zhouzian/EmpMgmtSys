9a7731830e97c10837b8f57c815ddfdbe4ea2c9c
ZIAN ZHOU <zhouzian@hotmail.com>
2018-07-06 10:27:05 -0400

    Enhanced test data generation.

--- PersistenceAccess/View/AppView.cs
+++ PersistenceAccess/View/AppView.cs
@@ -15,6 +15,13 @@ namespace PersistenceAccess.View
 {
 	public class AppView
 	{
+		#region Test data
+		private static Random random = new Random();
+		private static Array genders = Enum.GetValues(typeof(Gender));
+		private static Array titles = Enum.GetValues(typeof(Title));
+		private static Array levels = Enum.GetValues(typeof(Level));
+		private static Array firstNames = new string[] { "Tom", "Peter", "Michael", "Jared", "Robert", "William", "Greg", "Edward", "Karl", "Andrew", "David", "James", "Eveline", "Chris", "John", "Mary" };
+		private static Array lastNames = new string[] { "Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Lopez", "Hernandez", "Wilson", "Anderson", "Thomas", "Lee", "Perez", "White", "Clark", "Sanchez" };
 		public static void SeedDB()
 		{
 			using (var db = new LiteDatabase(Constants.DB_NAME))
@@ -25,28 +32,46 @@ namespace PersistenceAccess.View
 				{
 					db.DropCollection(collectionName);
 				}
+				
 				//setup test data
-				Employee mgr = EmployeeFactory.CreateEmployee(db, "Mike", "Zhang", Gender.FEMALE, "mike.zhang@laserfiche.com", new Employee() { Id = Guid.Empty }, Title.TECHNICAL_PRODUCT_MANAGER, Level.PRINCIPLE, 9999, 999, DateTime.Now);
-				Employee dude = EmployeeFactory.CreateEmployee(db, "Dude", "Huang", Gender.MALE, "dude.huang@laserfiche.com", mgr, Title.SOFTWARE_ENGINEER, Level.I, 100, 10, new DateTime(2018, 1, 12));
-				HistoryFactory.CreateHistory(db, dude, mgr, Title.SOFTWARE_ENGINEER, Level.III, 1000, 1000, ActionType.ANNUAL_PERFORMANCE_REVIEW, new DateTime(2010, 10, 12));
-
 				Employee nextMgr = new Employee() { Id = Guid.Empty };
-				for(int i = 0; i < 10; i++)
+				for(int i = 0; i < 99; i++)
 				{
-					nextMgr = EmployeeFactory.CreateEmployee(db, RandomString(5), RandomString(7), Gender.MALE, RandomString(10) + "@laserfiche.com", nextMgr, Title.SOFTWARE_ENGINEER_IN_TEST, Level.PRINCIPLE, 9999, 10, DateTime.Now);
-					HistoryFactory.CreateHistory(db, nextMgr, nextMgr, Title.TECHNICAL_PRODUCT_MANAGER, Level.III, 45000, 1000, ActionType.ANNUAL_PERFORMANCE_REVIEW, DateTime.Now.AddDays(2));
+					FakeName fname = GetAName();
+					Employee theNextMgr = EmployeeFactory.CreateEmployee(db, fname.FirstName, fname.LastName, (Gender)GetValue(genders), fname.Email, nextMgr, (Title)GetValue(titles), (Level)GetValue(levels), random.Next(50000, 200000), random.Next(0, 10000), DateTime.Now.AddMonths(-3));
+					HistoryFactory.CreateHistory(db, theNextMgr, nextMgr, (Title)GetValue(titles), (Level)GetValue(levels), random.Next(50000, 200000), random.Next(0, 10000), ActionType.ANNUAL_PERFORMANCE_REVIEW, DateTime.Now);
+					nextMgr = theNextMgr;
 				}
 				//validation
 			}
 		}
 
-		private static Random random = new Random();
-		private static string RandomString(int length)
+		private struct FakeName
 		{
-			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-			return new string(Enumerable.Repeat(chars, length)
-			  .Select(s => s[random.Next(s.Length)]).ToArray());
+			public string FirstName { get; set; }
+			public string LastName { get; set; }
+			public string Email
+			{
+				get
+				{
+					return FirstName.ToLower() + "." + LastName.ToLower() + "@fakeemail.com";
+				}
+			}
 		}
+
+		private static FakeName GetAName()
+		{
+			string firstName = (string)firstNames.GetValue(random.Next(firstNames.Length));
+			string lastName = (string)lastNames.GetValue(random.Next(lastNames.Length));
+			return new FakeName() { FirstName = firstName, LastName = lastName };
+		}
+
+		private static object GetValue(Array enumArray)
+		{
+			return enumArray.GetValue(random.Next(enumArray.Length));
+		}
+
+		#endregion
 
 		public static ListViewItem[] GetMainListView(VisibilityParam visibParam)
 		{
