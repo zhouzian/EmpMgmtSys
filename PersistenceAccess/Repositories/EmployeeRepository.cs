c3a42af0bc2fee3e1f8987588599df6a64d341c3
ZIAN ZHOU <zhouzian@hotmail.com>
2019-06-13 15:00:18 +0800

    Removed unused using statement

--- PersistenceAccess/Repositories/EmployeeRepository.cs
+++ PersistenceAccess/Repositories/EmployeeRepository.cs
@@ -5,8 +5,6 @@ using PersistenceAccess.Factories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PersistenceAccess.Repositories
 {
